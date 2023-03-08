0a61f5f29078a2ea28338e1445cbe33e5371c9d0
Ericwiz <wisdomagu44@gmail.com>
2023-03-08 13:56:58 +0000

    Revert "finished setup and created db migrations"
    This reverts commit 9c0b63ed6d781c5f16095ecb5dc91a7013aea98e.

--- migrations/sqls/20230308133055-tasks-up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE tasks (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR,
-    descriprion text,
-    completed BOOLEAN,
-    user_id bigint REFERENCES  users(id),
-    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
-)
